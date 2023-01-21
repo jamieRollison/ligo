c087c141e87ebec471f409bf548d4df8e474eb79
jamieRollison <rollisonjamie15@gmail.com>
2023-01-21 14:19:56 -0500

    keys issue

--- ligo/auth.config.js
+++
@@ -1,8 +0,0 @@
-const authConfig = {
-	domain: 'dev-n1mrvffjfh2zld1i.us.auth0.com',
-	clientId: 'IjbijoUMVKIJgUIFFGrfJrNJg3CBXFUp'
-};
-
-const DATABASE_URL = 'postgresql://jamie:LjpZ4W8AtjFL8LilPae3Wg@boilermake-ligo-8285.7tt.cockroachlabs.cloud:26257/ligo?sslmode=verify-full';
-
-export { authConfig, DATABASE_URL };
