427ef565c93ffe0ae427eb66c3984cc544f0e47d
estrella <epopo2@illinois.edu>
2023-01-21 02:53:06 -0500

    Fix

--- ligo/src/lib/components/SearchBarAndButton.svelte
+++ ligo/src/lib/components/SearchBarAndButton.svelte
@@ -10,7 +10,7 @@
     { state: "Welding Convention of Michigan" },
   ];
 
-  const extract = (item) => item.state;
+  const extract = (/** @type {{ state: any; }} */ item) => item.state;
 </script>
 
 
@@ -26,7 +26,7 @@
     />
 
     <!-- TODO: fix positioning, colors -->
-    <button class="bg-white/50 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4">
+    <button class="font-WorkSans bg-white/50 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4">
         Couldn't find your event? Create a new page for it!
     </button>
 </div>
