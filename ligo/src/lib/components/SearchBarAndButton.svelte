34263715ff0f719eca73046edf0adbeaa6cb2b23
Liza George <liza@chiramattel.com>
2023-01-21 02:49:14 -0500

    fixed ugly highlighting of search bar

--- ligo/src/lib/components/SearchBarAndButton.svelte
+++ ligo/src/lib/components/SearchBarAndButton.svelte
@@ -10,23 +10,32 @@
     { state: "Welding Convention of Michigan" },
   ];
 
-  const extract = (item) => item.state;
+  const extract = (/** @type {{ state: any; }} */ item) => item.state;
 </script>
 
 
 
 <div class = "flex flex-col my-10">
-    <!-- TODO: fix ugly highlighting - https://github.com/metonym/svelte-typeahead#custom-styled-results -->
     <!-- TODO: add icon -->
-    <Typeahead
+    <Typeahead class="rounded"
         hideLabel
         placeholder={`Search for your event...`}
         {data} 
         {extract} 
     />
 
-    <!-- TODO: fix positioning, colors -->
+    <!-- TODO: fix colors -->
     <button class="bg-white/50 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow mt-4">
         Couldn't find your event? Create a new page for it!
     </button>
 </div>
+
+<style>
+    :global(mark) {
+        background-color: #d5f0f2;
+    }
+
+    :global(li.selected, li:hover) {
+        background-color: #dbe1e3 !important ;
+    }
+</style>
