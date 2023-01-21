e20157288a78c26dd35cc663c6a3a6c8adfb4923
Liza George <liza@chiramattel.com>
2023-01-21 01:33:59 -0500

    Introduced button

---
+++ ligo/src/lib/components/SearchBarAndButton.svelte
@@ -0,0 +1,7 @@
+
+<div>
+    <!-- TODO: fix positioning, colors -->
+    <button class="bg-white/50 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        Couldn't find your event? Create a new page for it!
+    </button>
+</div>
--- ligo/src/routes/+page.svelte
+++ ligo/src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<div class="flex w-max m-auto">
-    <h1 class="font-Jomhuria text-9xl text-white">
-        Ligo
-    </h1>
-</div>
+<script>
+    import SearchBarAndButton from "$lib/components/SearchBarAndButton.svelte";
+</script>
+
+<main>
+    <div class="flex w-max m-auto">
+        <h1 class="font-Jomhuria text-9xl text-white">
+            Ligo
+        </h1>
+
+        <SearchBarAndButton/>
+    </div>
+</main>
