daed6e3dbe0b006966adea6979d27d8f26b955b0
Liza George <liza@chiramattel.com>
2023-01-21 01:56:22 -0500

    Imported typeahead searchbar

--- ligo/src/lib/components/SearchBarAndButton.svelte
+++ ligo/src/lib/components/SearchBarAndButton.svelte
@@ -1,7 +1,29 @@
 
-<div>
+<script>
+  import Typeahead from "svelte-typeahead";
+
+  const data = [
+    { state: "WWDC 2019" },
+    { state: "Grace Hopper 2022" },
+    { state: "BoilerMake X" },
+    { state: "HackIllinois" },
+    { state: "Welding Convention of Michigan" },
+  ];
+
+  const extract = (item) => item.state;
+</script>
+
+<Typeahead 
+    hideLabel
+    {data} 
+    {extract} 
+/>
+
+
+<main>
     <!-- TODO: fix positioning, colors -->
     <button class="bg-white/50 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
         Couldn't find your event? Create a new page for it!
     </button>
-</div>
+
+</main>
