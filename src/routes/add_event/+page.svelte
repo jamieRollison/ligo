061002b9cd68111378b7bc4fb78e7dfd02453de7
Liza George <liza@chiramattel.com>
2023-01-21 20:31:10 -0500

    Fixed centering of buttons

--- src/routes/add_event/+page.svelte
+++ src/routes/add_event/+page.svelte
@@ -1,21 +1,25 @@
 <!-- TODO: styling - add another graphic at the top of this page - maybe svgs around the whole thing? -->
 <div class="my-10 md:mx-40 lg:mx-60">
-	<div>
-		<h1 class="font-Jomhuria text-6xl text-white flex justify-center text-center">
-			Are you an organizer or attendee of this event?
-		</h1>
-	</div>
+  <div>
+    <h1
+      class="font-Jomhuria text-6xl text-white flex justify-center text-center"
+    >
+      Are you an organizer or attendee of this event?
+    </h1>
+  </div>
 
-	<div class="flex flex-col space-y-2 md:flex-row md:justify-center md:space-x-8">
-		<a
-			class="w-48 font-WorkSans bg-white/50 hover:bg-gray-100 rounded text-gray-800 py-2 px-4 mt-2"
-			href="/add_event/organizer"
-			>Organizer
-		</a>
-		<a
-			class="w-48 font-WorkSans  bg-white/50 hover:bg-gray-100 rounded text-gray-800 py-2 px-4 mt-2"
-			href="/add_event/attendee"
-			>Attendee
-		</a>
-	</div>
+  <div
+    class="flex flex-col space-y-2 md:flex-row md:justify-center md:space-x-8"
+  >
+    <a
+      class="w-48 text-center font-WorkSans bg-white/50 hover:bg-gray-100 rounded text-gray-800 py-2 px-4 mt-2"
+      href="/add_event/organizer"
+      >Organizer
+    </a>
+    <a
+      class="w-48 text-center font-WorkSans  bg-white/50 hover:bg-gray-100 rounded text-gray-800 py-2 px-4 mt-2"
+      href="/add_event/attendee"
+      >Attendee
+    </a>
+  </div>
 </div>
