a608a0948381f6b24f703938ff3c98d6a67368d2
jamieRollison <rollisonjamie15@gmail.com>
2023-01-21 20:12:19 -0500

    marked dynamic event pages non-prerenderable

--- .svelte-kit/generated/nodes/3.js
+++ .svelte-kit/generated/nodes/3.js
@@ -1,1 +1,3 @@
+import * as universal from "../../../src/routes/[event_id]_info_page/+page.js";
+export { universal };
 export { default as component } from "../../../src/routes/[event_id]_info_page/+page.svelte";
