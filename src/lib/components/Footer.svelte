4caecf43e42aa4884912708d3eb6646ac56e7ed1
estrella <epopo2@illinois.edu>
2023-01-21 23:17:55 -0500

    Add about page

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer class="flex flex-col items-center">
+    <div class="flex flex-row items-center font-WorkSans text-white hover:text-pink-100">
+        Made with <span><img src="assets/logo.png" alt="Ligo heart recycle logo" class="w-10"></span> by Liza, Jamie, and Ellie
+    </div>
+    <div class="text-white font-WorkSans">
+        @ BoilerMake 2023
+    </div>
+</footer>
+
+<style>
+    footer {
+        position: absolute;
+        bottom: 0;
+        height: 100px;
+        width: 100%;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script>
 	import '$lib/styles/app.css';
 	import Nav from '$lib/components/Nav.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <main>
 	<Nav />
 	<slot />
+	<div class="flex justify-center">
+	<Footer />
+	</div>
 </main>
+
+
